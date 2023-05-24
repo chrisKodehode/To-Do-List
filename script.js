be5c31f907dc30d0956d0b353d195806ee708016
haugrud123 <chrishaugrud111@gmail.com>
2023-05-24 11:39:23 +0200

    + Redid code in a test project | + Fixed save bugs

--- script.js
+++ script.js
@@ -1,170 +1,119 @@
 /*
     TODO:
-    - Fix todo list save
-    - Fix multiple save bug  
-    - Fix save bug when multiple items is selected
+    - Fix todo list save storage thingy
 */
-
 
 // Define html elements and assign them to a variable
 let todoContainer = document.getElementById("todo-container");
 let htmlForm = document.getElementById("todo-form");
 let inputBox = document.getElementById("todo-input");
-let buttonEdit = document.createElement("button");
-let buttonDelete = document.createElement("button");
-let divText  = document.createElement("div");
-
-function saveEdit(divText, editInput, buttonEdit, buttonSave) {
-    divText.textContent = editInput.value;
-    
-    // Show the edit button
-    buttonEdit.style.display = "inline-block";
-    // Hide the save button
-    buttonSave.style.display = "none";
-}
 
 htmlForm.addEventListener("submit", function(event){
-    // Prevent page refresh on click
     event.preventDefault();
 
     let inputElement = inputBox;
-        if (inputElement.value !== "") {
-            // Create list item and div for the text
-            listItem = document.createElement("li");
-            divText  = document.createElement("div");
+    if (inputElement.value !== "") {
+        // Create list item and div for the text
+        let listItem = document.createElement("li");
+        listItem.style.display = "item-list";
+        listItem.style.marginTop = "10px";
+        
+        let divText  = document.createElement("div");
+        divText.style.fontSize = "16px";
+        divText.style.display = "inline-block";
+        divText.style.marginBottom = "10px";
+        listItem.appendChild(divText);
 
-            listItem.style.display = "item-list";
-            listItem.style.marginTop = "10px";
+        let inputValue = inputElement.value;
+        divText.textContent = inputValue;
 
-            // Apply CSS Style to listItems
-            divText.style.fontSize = "16px";
-            divText.style.display = "inline-block";
-            divText.style.marginBottom = "10px";
-            listItem.appendChild(divText);
+        let buttonEdit = document.createElement("button");
+        buttonEdit.textContent = "Edit";
+        // Apply CSS styles
+        buttonEdit.style.backgroundColor = "yellow";
+        buttonEdit.style.color = "black";
 
-            // Retrieve input value
-            let inputValue = inputElement.value;
+        buttonEdit.style.marginRight = "100%";
+        buttonEdit.style.padding = "10px 30px";
 
-            // Display input value in the divText
-            divText.textContent = inputValue;
+        buttonEdit.style.border = "none";
+        buttonEdit.style.borderRadius = "5px";
 
-            // Create div for the buttons
-            let divButton = document.createElement("div");
-            listItem.appendChild(divButton)
+        buttonEdit.style.boxShadow = "black 0px 0px 1px"
 
-            // Create a button for editing
-            let buttonEdit = document.createElement("button");
+        buttonEdit.style.cursor = "pointer";
 
-            // Add text to the button
-            buttonEdit.textContent = "Edit"
+        listItem.appendChild(buttonEdit);
 
-            // Apply CSS styles
-            buttonEdit.style.backgroundColor = "yellow";
-            buttonEdit.style.color = "black";
+        let buttonSave = document.createElement("button");
+        buttonSave.textContent = "Save";
+        // Apply CSS styles
+        buttonSave.style.backgroundColor = "#33ff00";
+        buttonSave.style.color = "white";
+        
+        buttonSave.style.display = "none";
+        buttonSave.style.marginRight = "100%";
+        buttonSave.style.padding = "10px 26px";
 
-            buttonEdit.style.marginRight = "5px";
-            buttonEdit.style.padding = "10px 30px";
+        buttonSave.style.border = "none";
+        buttonSave.style.borderRadius = "5px";
 
-            buttonEdit.style.border = "none";
-            buttonEdit.style.borderRadius = "5px";
-            buttonEdit.style.boxShadow = "black 0px 0px 1px"
+        buttonSave.style.boxShadow = "black 0px 0px 1px";
+        buttonSave.style.cursor = "pointer";
 
-            buttonEdit.style.cursor = "pointer";
 
-            // Append the buttonEdit to the list item
-            listItem.appendChild(buttonEdit);
+        buttonSave.style.display = "none";
+        listItem.appendChild(buttonSave);
 
-            // Create a button for saving
-            let buttonSave = document.createElement("button");
+        let buttonDelete = document.createElement("button");
+        buttonDelete.textContent = "Delete";
+        // Apply CSS styles
+        buttonDelete.style.backgroundColor = "red";
+        buttonDelete.style.color = "black";
 
-            // Add text to the button
-            buttonSave.textContent = "Save";
+        buttonDelete.style.marginRight = "100%"
+        buttonDelete.style.marginTop = "10px"
+        buttonDelete.style.padding = "10px 22px";
 
-            // Apply CSS styles
-            buttonSave.style.backgroundColor = "#33ff00";
-            buttonSave.style.color = "white";
+        buttonDelete.style.border = "none";
+        buttonDelete.style.borderRadius = "5px";
+
+        buttonDelete.style.boxShadow = "black 0px 0px 1px"
+
+        buttonDelete.style.cursor = "pointer";
+
+        listItem.appendChild(buttonDelete);
+
+        buttonDelete.addEventListener("click", function(){
+            listItem.remove();
+        });
+
+        buttonEdit.addEventListener("click", function(){
+            let editInput = document.createElement("input");
+            editInput.type = "text";
+            editInput.value = divText.textContent;
+
+            divText.innerHTML = '';
+            divText.appendChild(editInput);
+
+            buttonEdit.style.display = "none";
+            buttonSave.style.display = "inline-block";
+
+            editInput.focus();
+
+            // Add a blur event listener to save the edited text
+            editInput.addEventListener("blur", function () {
+                // Update the divText with the edited value
+                divText.textContent = editInput.value;
             
-            buttonSave.style.display = "none";
-            buttonSave.style.marginRight = "5px";
-            buttonSave.style.padding = "10px 30px";
-
-            buttonSave.style.border = "none";
-            buttonSave.style.borderRadius = "5px";
-            buttonSave.style.boxShadow = "black 0px 0px 1px";
-
-            buttonSave.style.cursor = "pointer";
-            // Append the buttonSave to the list item
-            listItem.appendChild(buttonSave);
-
-            // Create a button for deleting
-            let buttonDelete = document.createElement("button");
-
-            // Add text to the button
-            buttonDelete.textContent = "Delete"
-
-            // Apply CSS styles
-            buttonDelete.style.backgroundColor = "red";
-            buttonDelete.style.color = "black";
-
-            buttonDelete.style.padding = "10px 20px";
-
-            buttonDelete.style.border = "none";
-            buttonDelete.style.borderRadius = "5px";
-            buttonDelete.style.boxShadow = "black 0px 0px 1px"
-
-            buttonDelete.style.cursor = "pointer";
-
-            // Append the buttonDelete to the list item
-            listItem.appendChild(buttonDelete);
-
-            // If delete button is clicked, remove the list item
-            buttonDelete.addEventListener("click", function(event){
-                if (event.target === buttonDelete) {
-                    listItem.remove();
-                }
-            })
-
-            // If edit button is clicked, replace text
-            buttonEdit.addEventListener("click", function(event){
-                if (event.target === buttonEdit) {
-                    // Create a new input field for editing
-                    let editInput = document.createElement("input");
-
-                    // Set the input field type to "text"
-                    editInput.type = "text";
-
-                    editInput.value = divText.textContent;
-
-                    // Clear the value of divText
-                    divText.innerHTML = '';
-                
-                    divText.appendChild(editInput);
-
-                    // hide the edit button
-                    buttonEdit.style.display = "none";
-                    // Show the save button
-                    buttonSave.style.display = "inline-block";
-
-                    // Focus the input field
-                    editInput.focus();
-
-                    // Add a blur event listener to save the edited text
-                    editInput.addEventListener("blur", function () {
-                    // Update the divText with the edited value
-                    divText.textContent = editInput.value;
-                
-                    // Show the edit button
-                    buttonEdit.style.display = "inline-block";
-                    // Hide the save button
-                    buttonSave.style.display = "none";
-                    });
-                }
+                // Show the edit button
+                buttonEdit.style.display = "inline-block";
+                // Hide the save button
+                buttonSave.style.display = "none";
             });
+        });
 
-            // Append the listItem to the todoContainer
-            todoContainer.appendChild(listItem);
-
-            // Clear the input value
-            inputElement.value = '';
-        }
+        todoContainer.appendChild(listItem);
+        inputElement.value = '';
+    }
 });
